6d93dccc1d2d7fdf5159bad654ce180fa651b5d7
Anthony Castro <anthony618@gmail.com>
2024-01-21 16:56:01 -0500

    Set up database

---
+++ src/server/database/index.ts
@@ -0,0 +1,36 @@
+import Dexie, { DexieOptions } from "dexie";
+
+const DATABASE_NAME = "TinyRecipeBoxDB";
+
+type User = {
+  createdAt: Date;
+  id: string;
+};
+
+export class AppDatabase extends Dexie {
+  users!: Dexie.Table<User, string>;
+
+  constructor(dexieOptions: DexieOptions | undefined) {
+    super(DATABASE_NAME, dexieOptions);
+
+    this.version(1).stores({
+      users: "&id",
+    });
+  }
+}
+
+let database: AppDatabase | undefined;
+
+export const initializeDatabase = (dexieOptions?: DexieOptions) => {
+  database = new AppDatabase(dexieOptions);
+
+  return database;
+};
+
+export const getDatabase = () => {
+  if (!database) {
+    throw new Error("Database has not been initialized.");
+  }
+
+  return database;
+};
