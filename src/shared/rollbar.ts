299f8679b4639093283c4a6c180a95a441537ff2
Anthony Castro <anthony618@gmail.com>
2024-01-24 18:00:57 -0500

    Fix rollbar error

--- src/shared/rollbar.ts
+++ src/shared/rollbar.ts
@@ -4,5 +4,6 @@ export const rollbarConfig = {
   accessToken: config.ROLLBAR_ACCESS_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
+  enabled: config.ROLLBAR_ACCESS_TOKEN !== undefined,
   environment: "development",
 };
