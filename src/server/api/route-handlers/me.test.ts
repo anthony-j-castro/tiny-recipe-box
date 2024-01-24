4091ceaf71d532c5fa16938d4a2a0dddaf56e664
Anthony Castro <anthony618@gmail.com>
2024-01-24 01:07:10 -0500

    Add more tests

--- src/server/api/route-handlers/me.test.ts
+++ src/server/api/route-handlers/me.test.ts
@@ -1,7 +1,7 @@
 import { IDBKeyRange, indexedDB } from "fake-indexeddb";
 import StatusCode from "status-code-enum";
 import { v4 as uuidv4 } from "uuid";
-import { getMe } from "./me";
+import { createMe, getMe } from "./me";
 
 describe("server/api/route-handlers/me", () => {
   beforeEach(async () => {
@@ -40,6 +40,64 @@ describe("server/api/route-handlers/me", () => {
         body: {
           createdAt: user.createdAt,
           userId: user.id,
+        },
+      });
+    });
+
+    it("returns a not found error when 0 records exist", async () => {
+      const { getDatabase } = await import("~/server/database");
+      const db = getDatabase();
+
+      const meResponse = await getMe(db);
+
+      expect(meResponse).toEqual({
+        status: StatusCode.ClientErrorNotFound,
+        body: {
+          message: "User not found.",
+        },
+      });
+    });
+
+    it("returns a server error when > 1 records exist", async () => {
+      const { getDatabase } = await import("~/server/database");
+      const db = getDatabase();
+
+      await db.users.bulkAdd([
+        { createdAt: new Date(), id: uuidv4() },
+        { createdAt: new Date(), id: uuidv4() },
+      ]);
+
+      const meResponse = await getMe(db);
+
+      expect(meResponse).toEqual({
+        status: StatusCode.ServerErrorInternal,
+        body: {
+          message:
+            "Cannot determine current user. Unexpected number of users found.",
+        },
+      });
+    });
+  });
+
+  describe("createMe", () => {
+    beforeAll(() => {
+      jest.useFakeTimers().setSystemTime();
+    });
+
+    afterAll(() => {
+      jest.useRealTimers();
+    });
+
+    it("returns a new user", async () => {
+      const { getDatabase } = await import("~/server/database");
+      const db = getDatabase();
+      const meResponse = await createMe(db);
+
+      expect(meResponse).toEqual({
+        status: StatusCode.SuccessCreated,
+        body: {
+          createdAt: new Date(),
+          userId: expect.any(String),
         },
       });
     });
