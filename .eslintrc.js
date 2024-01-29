38957d7c8086f96d7891e393fe6ae946b696c5b2
Anthony Castro <anthony-j-castro@users.noreply.github.com>
2024-01-29 00:11:03 -0500

    chore: Remove redudant eslint rules (#33)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,6 @@ module.exports = {
     "@anthony-j-castro/eslint-config",
     "plugin:@tanstack/eslint-plugin-query/recommended",
   ],
-  plugins: ["sort-destructure-keys"],
-  rules: {
-    "arrow-body-style": ["error", "as-needed"],
-    quotes: [
-      "error",
-      "double",
-      { allowTemplateLiterals: false, avoidEscape: true },
-    ],
-    "sort-destructure-keys/sort-destructure-keys": [
-      "error",
-      { caseSensitive: false },
-    ],
-  },
   settings: {
     "import/resolver": {
       alias: {
