41e9ec5dad69b9e766c27634af2867fa72be00c2
Anthony Castro <anthony618@gmail.com>
2024-02-03 10:10:06 -0500

    Force e2e failure

--- cypress/e2e/consent.cy.ts
+++ cypress/e2e/consent.cy.ts
@@ -1,6 +1,6 @@
 describe("The consent modal", () => {
   it("successfully loads", () => {
     cy.visit("http://localhost:3000");
-    cy.get('[data-cy="consent"]').should("exist");
+    cy.get('[data-cy="consents"]').should("exist");
   });
 });
