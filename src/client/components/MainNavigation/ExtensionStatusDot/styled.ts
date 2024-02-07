9a409d1fb44be044cd7ba7a84dbfe72f45d3ed3b
Anthony Castro <anthony618@gmail.com>
2024-02-06 19:53:37 -0500

    Update status dot styles

--- src/client/components/MainNavigation/ExtensionStatusDot/styled.ts
+++ src/client/components/MainNavigation/ExtensionStatusDot/styled.ts
@@ -4,8 +4,17 @@ export const Dot = styled.span<{ $status: boolean | null }>`
   display: inline-block;
   width: 12px;
   height: 12px;
-  border-radius: 50%;
   background: ${(props) =>
-    props.$status === null ? "gray" : props.$status ? "green" : "red"};
+    props.$status === null
+      ? "transparent"
+      : props.$status
+        ? "#ccff00"
+        : "#ff3131"};
+  border: 1px solid #818188;
+  border-radius: 50%;
   margin-left: 8px;
+
+  [data-status="active"] > & {
+    border: 1px solid #ffffff;
+  }
 `;
